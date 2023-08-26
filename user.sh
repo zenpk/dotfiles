dc37026d5858c5cdeccb9efb610ff03371bcaf27
zenpk <99558412+zenpk@users.noreply.github.com>
2023-08-27 00:32:01 +0800

    update

--- user.sh
+++ user.sh
@@ -59,7 +59,9 @@ EOF
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" << "EOF"
 Y
 EOF
+sudo sed s/required/sufficient/g -i /etc/pam.d/chsh
 sudo chsh -s $(which zsh) $(whoami)
+sudo sed s/sufficient/required/g -i /etc/pam.d/chsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
