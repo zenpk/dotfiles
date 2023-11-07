fd5a09e46bbae756b9994a776efba7703c7a8c21
zenpk <99558412+zenpk@users.noreply.github.com>
2023-11-07 14:30:07 +0800

    fix dollar sign

--- ubuntu-bootstrap/user.sh
+++ ubuntu-bootstrap/user.sh
@@ -121,22 +121,22 @@ cat << EOF >> ~/.zshrc
 
 function lazygit() {
 	git add .
-	git commit -m "$*"
+	git commit -m "\$*"
 	git push
 }
 
 function gitclonessh() {
-    git clone "$1" --config core.sshCommand="ssh -i ~/.ssh/$2"
+    git clone "\$1" --config core.sshCommand="ssh -i ~/.ssh/\$2"
 }
 
 function gitsignssh() {
     git config gpg.format ssh
-    git config user.signingkey "~/.ssh/$1.pub"
+    git config user.signingkey "~/.ssh/\$1.pub"
     git config commit.gpgsign true
 }
 
 function gitcommit() {
-    git commit -m "$*"
+    git commit -m "\$*"
 }
 
 export PATH="$PATH:/snap/bin"
