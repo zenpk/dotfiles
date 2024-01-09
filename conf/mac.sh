44986cd2cd48697e5b1ce73255a1d3a60466d7e4
Zen <99558412+zenpk@users.noreply.github.com>
2024-01-09 16:58:28 +0800

    Update mac.sh

--- conf/mac.sh
+++ conf/mac.sh
@@ -1,3 +1,15 @@
+# hold key
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
+
+# clear proxy
+networksetup -setwebproxystate Wi-Fi off
+networksetup -setsecurewebproxystate Wi-Fi off
+export http_proxy=""
+export https_proxy=""
+export JAVA_OPTS="$JAVA_OPTS -DsocksProxyPort"
+echo done
+
+# mount nfs
+sudo mount -o hard,intr,rsize=8192,wsize=8192,bg,tcp example.com:/apps /System/Volumes/Data/build/apps
