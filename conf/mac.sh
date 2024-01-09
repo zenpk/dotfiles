2a6620673b6ab0fb5a73f1c77443fea09ff7ec22
Zen <99558412+zenpk@users.noreply.github.com>
2024-01-09 16:03:59 +0800

    Update mac.sh

--- conf/mac.sh
+++ conf/mac.sh
@@ -1,4 +1,3 @@
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
-
+defaults write -g InitialKeyRepeat -int 15
+defaults write -g KeyRepeat -int 2
