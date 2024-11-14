504880b82f52f0b337ebee6c20c8286564de8527
zenpk <99558412+zenpk@users.noreply.github.com>
2024-11-15 00:23:01 +0800

    add comments on windows keyboard

--- conf/shortcuts.ahk
+++ conf/shortcuts.ahk
@@ -8,6 +8,9 @@ PgDn::WheelDown
 
 End::^c
 Insert::^v
+; when using Japanese IME, using Caps+Any will cause the Ctrl to be in the held state
+; press the normal Ctrl once can solve this
+; a better solution would be getting a sane keyboard
 CapsLock::LCtrl
 NumLock::#Space
 #Left::#^Left
--- scripts/windows.ps1
+++ scripts/windows.ps1
@@ -2,8 +2,8 @@
 # https://superuser.com/questions/1058474/increase-keyboard-repeat-rate-beyond-control-panel-limits-in-windows-10
 Set-Location "HKCU:\Control Panel\Accessibility\Keyboard Response"
 
-Set-ItemProperty -Path . -Name AutoRepeatDelay       -Value 225
-Set-ItemProperty -Path . -Name AutoRepeatRate        -Value 45
-Set-ItemProperty -Path . -Name DelayBeforeAcceptance -Value 0
-Set-ItemProperty -Path . -Name BounceTime            -Value 0
-Set-ItemProperty -Path . -Name Flags                 -Value 3
+Set-ItemProperty -Path . -Name AutoRepeatDelay       -Value 225 # default 1000
+Set-ItemProperty -Path . -Name AutoRepeatRate        -Value 45 # 500
+Set-ItemProperty -Path . -Name DelayBeforeAcceptance -Value 0 # 1000
+Set-ItemProperty -Path . -Name BounceTime            -Value 0 # 0
+Set-ItemProperty -Path . -Name Flags                 -Value 3 # 126
