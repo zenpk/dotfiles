ba153b50c4b4d2126894caf6e2c4e58b36939480
zenpk <99558412+zenpk@users.noreply.github.com>
2025-01-09 02:40:28 +0800

    rm omz

--- bootstrap/setup.sh
+++ bootstrap/setup.sh
@@ -1,14 +1,165 @@
 #!/bin/bash
-# this script is for setup the user and should be run by root
 
-# add user
-read -p "Enter username: " username
-read -s -p "Enter password: " password
-echo
-useradd -m "$username" # -m flag creates the user's home directory
-echo "$username:$password" | chpasswd
-usermod -aG sudo "$username"
-echo "$username ALL=(ALL) NOPASSWD:ALL" | tee "/etc/sudoers.d/$username"
-echo "AllowUsers $username" | tee -a /etc/ssh/sshd_config
+# apt
+sudo apt update
+sudo apt install -y zsh
+sudo apt install -y tmux
+sudo apt install -y vim-gtk3
 
-echo "done. now login as the user and run user.sh"
+# homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install fzf
+
+# tmux
+cat << EOF > ~/.tmux.conf
+# change prefix to C-a
+unbind C-b
+set-option -g prefix C-a
+bind-key C-a send-prefix
+
+set -sg escape-time 10 # prevent 0;10;1c
+# set-environment -g TZ UTC
+# address vim mode switching delay (http://superuser.com/a/252717/65504)
+set -s escape-time 0
+# increase tmux messages display duration from 750ms to 3s
+set -g display-time 3000
+# refresh 'status-left' and 'status-right' more often, from every 15s to 5s
+set -g status-interval 5
+# focus events enabled for terminals that support them
+set -g focus-events on
+set -g mode-keys vi
+set -g status-keys vi
+set -g mouse off
+set -g history-limit 10000 
+set -g default-terminal 'tmux-256color'
+set -ag terminal-overrides ",xterm-256color:RGB"  # correct color
+# mac
+# set -g default-terminal 'screen-256color'
+
+# vim-like pane switching
+bind -r k select-pane -U
+bind -r j select-pane -D
+bind -r h select-pane -L
+bind -r l select-pane -R
+
+# use v to trigger selection    
+bind -T copy-mode-vi v send-keys -X begin-selection
+# use y to yank current selection
+bind -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+# easier and faster switching between next/prev window
+bind -r C-p previous-window
+bind -r C-n next-window
+# above bindings enhance the default prefix + p and prefix + n bindings by allowing you to hold Ctrl and repeat a + p/a + n (if your prefix is C-a), which is a lot quicker.
+bind -r a last-window
+
+# list of plugins
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-resurrect'
+
+set -g @plugin 'dracula/tmux'
+set -g @dracula-plugins "cpu-usage ram-usage time"
+set -g @dracula-show-powerline true
+set -g @dracula-show-flags true
+set -g @dracula-show-left-icon session
+set -g @dracula-day-month false
+set -g @dracula-show-timezone false
+set -g @dracula-military-time true
+
+# should be at the bottom
+run '~/.tmux/plugins/tpm/tpm'
+
+EOF
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# vim
+mkdir -p ~/.vim/undo
+cat << EOF > ~/.vimrc
+syntax on
+set background=dark
+set number
+set relativenumber
+set ignorecase
+set smartcase
+set incsearch
+set nohlsearch
+set scrolloff=10
+set ts=4 sw=4 sts=4 expandtab smarttab
+set smartindent
+set nowrap
+set noswapfile
+set nobackup
+set undofile
+set undodir=$HOME/.vim/undo
+set updatetime=50
+set signcolumn=yes
+set clipboard=
+
+map <space> <leader>
+" sane clipboard
+nnoremap c "_c
+nnoremap C "_C
+nnoremap x "_x
+xnoremap x "_x
+nnoremap X "+x
+xnoremap X "+x
+nnoremap d "_d
+nnoremap dd "_dd
+noremap D "_D
+nnoremap <leader>d "+dd
+nnoremap <leader>D "+D
+nnoremap p "+p
+xnoremap p "_d"+P
+nnoremap P "+P
+xnoremap P "_d"+P
+nnoremap y "+y
+nnoremap yy "+yy
+" yank without jump
+xnoremap y "+ygv<esc>
+
+" redo
+nnoremap U <C-r>
+nnoremap Q <nop>
+" sane scroll
+nnoremap <C-u> Hzz
+nnoremap <C-d> Lzz
+noremap H ^
+noremap L $
+" visual block
+nnoremap <leader>v <C-v>
+" search and replace
+nnoremap <leader>ss /<C-r>+<CR>
+xnoremap <leader>ss "ay/<C-r>a<CR>
+nnoremap <leader>sr :%s/<C-r>+/<C-r>+/gc<left><left><left>
+xnoremap <leader>sr "ay:%s/<C-r>a/<C-r>a/gc<left><left><left>
+" useful
+nnoremap <leader>sudow :w !sudo tee > /dev/null %<CR>
+nnoremap <leader>paste :set paste<CR>
+nnoremap <leader>copy :set nonumber norelativenumber signcolumn=no wrap<CR>
+" enter is not useless
+nnoremap <CR> viw
+" easy motion
+noremap s <leader><leader>w
+noremap S <leader><leader>b
+EOF
+
+# zsh
+sudo sed s/required/sufficient/g -i /etc/pam.d/chsh
+sudo chsh -s $(which zsh) $(whoami)
+sudo sed s/sufficient/required/g -i /etc/pam.d/chsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+git clone https://github.com/Aloxaf/fzf-tab ~/.zsh/fzf-tab
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+
+# ssh
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+touch ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+
+# manual
+echo "done"
+echo "run source ~/.zshrc to do p10k configure"
+echo "run tmux source ~/.tmux.conf and press prefix + I to install tmux plugins"
+echo "paste your SSH public key to ~/.ssh/authorized_keys"
--- rc/.zshrc
+++ rc/.zshrc
@@ -1,16 +1,23 @@
-# minimal zshrc
-plugins=(
-	git
-	zsh-syntax-highlighting
-	zsh-autosuggestions
-    fzf-tab
-)
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
 
-source $ZSH/oh-my-zsh.sh
+source ~/powerlevel10k/powerlevel10k.zsh-theme
 
-export PATH="$HOME/.local/bin:/usr/local/go/bin:$PATH"
+# Linux only
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
 source <(fzf --zsh)
+autoload -U compinit; compinit
+source ~/.zsh/fzf-tab/fzf-tab.plugin.zsh
+
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+export PATH="$HOME/go/bin:/usr/local/bin:$HOME/.local/bin:/usr/local/go/bin:$PATH"
 
 # fzf file explorer
 function fcd () {
@@ -72,8 +79,25 @@ function fcd () {
     done
 }
 
+# https://www.reddit.com/r/zsh/comments/wq4mq8/zsh_history_isnt_working/
+export HISTFILE=$HOME/.zsh_history # location of the history file
+export HISTSIZE=2000 # current session's history limit, also following this https://unix.stackexchange.com/a/595475 $HISTSIZE should be at least 20% bigger than $SAVEHIST 
+export SAVEHIST=1000 # zsh saves this many lines from the in-memory history list to the history file upon shell exit
+setopt SHARE_HISTORY # allows multiple Zsh sessions to share the same command history 
+setopt APPENDHISTORY # ensures that each command entered in the current session is appended to the history file immediately after execution
+
+alias ll="ls -l --color=auto"
+
+# https://stackoverflow.com/questions/16727459/what-makes-ctrlq-work-in-zsh
+unsetopt flow_control
 bindkey "^q" fzf-cd-widget
-SAVEHIST=1000  # Save most-recent 1000 lines
-HISTFILE="$HOME/.zsh_history"
+
 export FZF_DEFAULT_OPTS="--cycle --no-mouse --no-height --layout=default --bind='ctrl-p:up,ctrl-n:down,ctrl-k:preview-up,ctrl-j:preview-down,tab:toggle-out' --preview-window=wrap"
 export BAT_THEME="OneHalfDark"
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
